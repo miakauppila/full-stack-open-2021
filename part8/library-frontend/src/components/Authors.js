80c9999098d7752395bf245dae4411fa4a442369
miakauppila <mia.kauppila@gmail.com>
2021-06-11 12:36:33 +0300

    start library-frontend (Ex. 8.8-8.12)

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, UPDATE_AUTHOR } from '../queries'
+import Select from 'react-select';
+
+const Authors = (props) => {
+  const [selectedName, setSelectedName] = useState(null);
+  const [bornYear, setBornYear] = useState('')
+  
+
+  const result = useQuery(ALL_AUTHORS)
+
+  const [updateAuthor] = useMutation(UPDATE_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }]
+  })
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const setBornTo = Number(bornYear)
+    const name=selectedName.value
+    updateAuthor({ variables: { name, setBornTo } })
+
+    setSelectedName(null)
+    setBornYear('')
+  }
+
+  if (!props.show) {
+    return null
+  }
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  //authors available only after result.loading false
+  const authors = result.data.allAuthors
+
+  const authorsOptions = authors.map(a => ({
+    "value" : a.name,
+    "label" : a.name
+  }))
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+      <h2>Set birthyear</h2>
+      <form onSubmit={handleSubmit}>
+        <p>
+        <Select
+          defaultValue={selectedName}
+          onChange={setSelectedName}
+          options={authorsOptions}
+        />
+        </p>
+        <div>
+          born
+          <input
+            value={bornYear}
+            onChange={({ target }) => setBornYear(target.value)}
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
+
+    </div>
+  )
+}
+
+export default Authors
---
+++ part8/library-frontend/src/components/Books.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_BOOKS } from '../queries'
+
+const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+
+  if (!props.show) {
+    return null
+  }
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
+  const books = result.data.allBooks
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              author
+            </th>
+            <th>
+              published
+            </th>
+          </tr>
+          {books.map(book =>
+            <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.published}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Books
